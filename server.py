6480f3bb231aae82ce253e76a5a21657083fa1e4
Anirudh42 <av.anirudh.96@gmail.com>
2022-12-05 23:40:44 -0500

    wip qa app

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from transformers import pipeline
 import json
 
@@ -20,13 +20,22 @@ def welcome():
 
 @app.route("/qa",methods=["GET","POST"])
 def answer_question():
-    question1 = "What is Westeros?"
-    question2 = "Who is Night's watch?"
-    context = full_text
-    answer1 = qa_model(question = question1, context = context)
-    answer2 = qa_model(question = question2, context = context)
-    response = [{"Question:":question1,"Answer:":answer1},{"Question:":question2,"Answer:":answer2}]
-    return json.dumps(response)
+    # if request.method=="GET":
+    #     # question1 = "What is Westeros?"
+    #     # question2 = "Who is Night's watch?"
+    #     # context = full_text
+    #     # answer1 = qa_model(question = question1, context = context)
+    #     # answer2 = qa_model(question = question2, context = context)
+    #     # response = [{"Question":question1,"Answer":answer1},{"Question":question2,"Answer":answer2}]
+    #     return json.dumps(response)
+    if request.method=="POST":
+        question = request.form['userinput']
+        answer = qa_model(question=question,context=full_text)
+        response = {"Context":full_text,"Question":question,"Answer":answer}
+        # response = json.dumps(response, sort_keys = True, indent = 4, separators = (',', ': '))
+        return render_template("user_input.html",jsonfile=json.dumps(response))
+    
+    return render_template("user_input.html")
 
 
 if __name__=="__main__":
